bd7713e745e7cffa5fe177fad27c80a5b9181851
iRubix <mhpazooki1989@yahoo.com>
2018-07-09 11:12:46 +0430

    Updated 11:11 7/9/2018

---
+++ app/src/main/java/com/example/administrator/rubix/CommonBillsActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.rubix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommonBillsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.common_bills_layout);
+    }
+}
