84f7e988204480b12d2b447184d5c1b8a1642129
iRubix <mhpazooki1989@yahoo.com>
2018-07-09 21:53:08 +0430

    Updated 11:11 7/9/2018

---
+++ app/src/main/java/com/example/administrator/rubix/CreditActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.rubix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credit_layout);
+    }
+}
